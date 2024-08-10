ab6b9bc85045d51f0cb5efa17d95fbfa600fffef
FlemingJR <flemingjohnrobert.jrf@gmail.com>
2024-08-10 11:17:52 -0400

    made window

--- EGProject/src/Main.cpp
+++ EGProject/src/Main.cpp
@@ -1,5 +1,43 @@
-#include <iostream>
+#include <Windows.h>
+
+LRESULT CALLBACK
+	WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT res{};
+	res = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return res;
+}
 
 int main() {
-	std::cout << "goodbye world ):" << std::endl;
+	HINSTANCE hInstance{ GetModuleHandle(0) };
+
+	const wchar_t windowClassName[]{ L"window class" };
+
+	WNDCLASSW windowClass{ .lpfnWndProc = WindowProc,
+						   .hInstance = hInstance,
+						   .lpszClassName = windowClassName };
+
+	RegisterClassW(&windowClass);
+
+	HWND hwnd{ CreateWindowExW(
+		0,
+		windowClassName,
+		L"App Name",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		hInstance,
+		0
+	) };
+
+	if (hwnd == 0) {
+		return 0;
+	}
+
+	ShowWindow(hwnd, 1);
+
+	while (true) {}
 }
